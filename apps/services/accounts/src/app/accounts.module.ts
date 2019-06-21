25307b24b11d9cfef648782edeca1010db69779f
Marcus S. Abildskov <msa@siteattention.com>
2019-06-21 16:31:35 +0200

    chore: remove unused stuff

--- apps/services/accounts/src/app/accounts.module.ts
+++ apps/services/accounts/src/app/accounts.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { GraphqlDistributedModule } from '@ngme/distributed';
 
-import { GqlAccountsConfigService } from './graphql-accounts-config.service';
 import { AccountsResolver } from './accounts.resolver';
 import { join } from 'path';
 
 @Module({
   imports: [
-    /*GraphQLModule.forRootAsync({
-      useClass: GqlAccountsConfigService,
-    }),*/
     GraphqlDistributedModule.forRoot({
       typePaths: [join(__dirname, '**/*.graphql')]
     }),
